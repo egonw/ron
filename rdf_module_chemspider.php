9c3119853dbbf23023671eec6e325c780a2803c1
Egon Willighagen <egonw@grant.dreamhost.com>
2009-03-24 02:05:02 -0700

    Added double loop, as there is a nasty redirect involved

--- rdf_module_chemspider.php
+++ rdf_module_chemspider.php
@@ -13,6 +13,8 @@
   if (strlen($inchi) === 0) {
     $inchi = "InChI=1/CH4/h1H4";
   }
+
+  $inchi = "InChI=1/CH4/h1H4"; 
 ?>
 
 <rdf:Description
@@ -24,17 +26,30 @@
 
 include_once("getcontent.php");
 
-$url = "http://inchis.chemspider.com/Resolver.aspx?q=" . urlencode($inchi);
+# because a nasty redirect is involved, it is a double loop :(
+
+$url = "http://inchis.chemspider.com/Resolver.aspx?q=" . $inchi;
+print "<!-- $url -->\n";
 $content = get_content($url);
-# aspx?id=1& 
-preg_match("/id=(\d*)/", $content, $matches, PREG_OFFSET_CAPTURE);
+# print "<!-- " . $content . " -->";
+# first, deal with the redirect
+preg_match("/%2fRecord.aspx%3fid%3d(\d*)/", $content, $matches, PREG_OFFSET_CAPTURE);
 
 foreach ($matches as $value) {
-  # pick only one, the last
-  echo "<!-- CSID found: " . $value[0] . " -->\n";
-  # $chebi = $value[0];
-  echo "<rdfomn:csid>" . $value[0] . "</rdfomn:csid>";
-  # echo "<owl:sameAs rdf:resource=\"http://bio2rdf.org/chebi:" . $value[0] . "\"/>";
+  if (!ereg("Record.aspx", $value[0])) {
+    $url2 = "http://inchis.chemspider.com/Record.aspx?id=" . $value[0];
+    $content2 = get_content($url2);
+    # print "<!-- " . $content2 . " -->";
+    # first, deal with the redirect
+    preg_match("/ctl00_ContentPlaceHolder1_CompoundViewControl1_HyperLink2.*www.chemspider.com\/(\d*)/", $content2, $matches2, PREG_OFFSET_CAPTURE); 
+    foreach ($matches2 as $value2) {
+      if (!ereg("chemspider.com", $value2[0])) {
+        echo "<!-- CSID found: " . $value2[0] . " -->\n";
+        echo "<rdfomn:csid>" . $value2[0] . "</rdfomn:csid>";
+        # echo "<owl:sameAs rdf:resource=\"http://bio2rdf.org/chebi:" . $value[0] . "\"/>";
+      }
+    }
+  }
 }
 
 ?>
